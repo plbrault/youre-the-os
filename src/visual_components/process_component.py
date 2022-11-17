580ef5609f1136f241cc1bec519176347eafaa23
Pier-Luc Brault <pier-luc@brault.me>
2022-11-16 22:41:23 -0500

    Change process text color

--- src/visual_components/process_component.py
+++ src/visual_components/process_component.py
@@ -11,7 +11,7 @@ class ProcessComponent(Drawable):
     def __init__(self, x, y):
         self._x = x
         self._y = y
-        self._text_surface = FONT_ARIAL_10.render('READY', False, Color.WHITE)
+        self._text_surface = FONT_ARIAL_10.render('READY', False, Color.BLACK)
 
     def draw(self, surface):
         pygame.draw.rect(surface, Color.GREEN, pygame.Rect(self._x, self._y, ProcessComponent.WIDTH, ProcessComponent.HEIGHT))
