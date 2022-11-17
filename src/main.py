829144bc6c780dcff2e5bd6c1afd4b4ca1fe6edd
Pier-Luc Brault <pier-luc@brault.me>
2022-11-16 20:53:03 -0500

    Create fullscren window

--- src/main.py
+++ src/main.py
@@ -2,22 +2,16 @@ import sys, pygame
 
 pygame.init()
 
-
-size = width, height = 320, 240
-
-speed = [2, 2]
+size = width, height = 1920, 1080
 
 black = 0, 0, 0
 
-
 screen = pygame.display.set_mode(size)
 
-
 while True:
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT: sys.exit()
 
-
     screen.fill(black)
+
+    pygame.display.flip()
