3ac43ad54ad47fa8c15a7dd375ca042dd1130736
Pier-Luc Brault <pier-luc@brault.me>
2022-11-17 22:51:00 -0500

    Refactor Game class

--- src/game.py
+++ src/game.py
@@ -8,44 +8,47 @@ class Game:
         pygame.init()
         pygame.font.init()
 
-        size = width, height = 1024, 768
+        self._game_objects = []
 
-        self._cpus = [
+        self.setup()
+        self.main_loop()
+
+    def setup(self):
+        size = width, height = 1024, 768
+        self._screen = pygame.display.set_mode(size)
+
+        cpus = [
             Cpu(1),
             Cpu(2),
             Cpu(3),
             Cpu(4),
         ]
-
-        for i, cpu in enumerate(self._cpus):
+        for i, cpu in enumerate(cpus):
             x = 50 + i * cpu.view.width + i * 5
             y = 50
             cpu.view.setXY(x, y)
+        self._game_objects.extend(cpus)        
 
-        self._process = Process()
-        self._process.view.setXY(50, 150)
-
-        self._screen = pygame.display.set_mode(size)
-
+        process = Process()
+        process.view.setXY(50, 150)
+        self._game_objects.append(process)
+    
+    def main_loop(self):
         while True:
             self.update(pygame.time.get_ticks())
-            self.render()
+            self.render()        
 
     def update(self, current_time):
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-        for cpu in self._cpus:
-            cpu.update(current_time)
-
-        self._process.update(pygame.time.get_ticks())
+        for game_object in self._game_objects:
+            game_object.update(current_time)
 
     def render(self):
         self._screen.fill(Color.BLACK)
 
-        for cpu in self._cpus:
-            cpu.render(self._screen)
-
-        self._process.render(self._screen)
+        for game_object in self._game_objects:
+            game_object.render(self._screen)
 
         pygame.display.flip()
