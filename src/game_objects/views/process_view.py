969e81c3da99f59b3078d104c2db986bbb8984f3
Pier-Luc Brault <pier-luc@brault.me>
2022-11-19 15:54:53 -0500

    Add emojis to express process states (#5)

--- src/game_objects/views/process_view.py
+++ src/game_objects/views/process_view.py
@@ -20,24 +20,35 @@ class ProcessView(Drawable):
 
     def draw(self, surface):
         color = Color.YELLOW
+        starvation_emoji = '🙂'
 
         if self._process.starvation_level == 0:
             color = Color.GREEN
+            starvation_emoji = '😀'
         elif self._process.starvation_level == 1:
             color = Color.YELLOW
+            starvation_emoji = '🙂'
         elif self._process.starvation_level == 2:
             color = Color.ORANGE
+            starvation_emoji = '😐'
         elif self._process.starvation_level == 3:
             color = Color.RED
+            starvation_emoji = '☹️'
         elif self._process.starvation_level == 4:
             color = Color.DARK_RED
+            starvation_emoji = '😭'
         elif self._process.starvation_level == 5:
             color = Color.DARKER_RED
+            starvation_emoji = '💀'
 
         pygame.draw.rect(surface, color, pygame.Rect(self._x, self._y, self.width, self.height))
+
+        starvation_emoji_surface = load_emoji(starvation_emoji, (32, 32))
+        surface.blit(starvation_emoji_surface, (self._x + 2, self._y + 2))
+
         pid_text_surface = FONT_ARIAL_10.render('PID ' + str(self._process.pid), False, Color.BLACK)
-        surface.blit(pid_text_surface, (self._x + 28, self._y + 5))
+        surface.blit(pid_text_surface, (self._x + 32, self._y + 5))
 
         if self._process.is_blocked:
-            emoji_surface = load_emoji('⏳', (32, 32))
-            surface.blit(emoji_surface, (self._x + 16, self._y + 20))
+            blocked_emoji_surface = load_emoji('⏳', (32, 32))
+            surface.blit(blocked_emoji_surface, (self._x + 28, self._y + 32))
