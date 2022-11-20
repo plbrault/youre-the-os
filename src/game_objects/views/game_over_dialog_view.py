608c4a0a0f1e7894b733545c32be0cdfb9037c81
Pier-Luc Brault <pier-luc@brault.me>
2022-11-20 17:11:38 -0500

    Add text to game over dialog

--- src/game_objects/views/game_over_dialog_view.py
+++ src/game_objects/views/game_over_dialog_view.py
@@ -3,12 +3,22 @@ import os
 
 from lib.drawable import Drawable
 from lib.ui.color import Color
-from lib.ui.fonts import FONT_ARIAL_20, FONT_ARIAL_10
+from lib.ui.fonts import FONT_ARIAL_16, FONT_ARIAL_30
+from game_objects.label import Label
 
 class GameOverDialogView(Drawable):
     def __init__(self, game_over_dialog):
         super().__init__()
+        
         self._image = pygame.image.load(os.path.join('assets', 'game-over-image.png')).convert_alpha()
+        
+        self._game_over_label = Label('GAME OVER')
+        self._game_over_label.font = FONT_ARIAL_30
+        self._game_over_label.color = Color.WHITE
+
+        self._explanation_label = Label('You made the user angry!')
+        self._explanation_label.font = FONT_ARIAL_16
+        self._explanation_label.color = Color.WHITE
 
     @property
     def width(self):
@@ -19,6 +29,15 @@ class GameOverDialogView(Drawable):
         return 700
 
     def draw(self, surface):
-        pygame.draw.rect(surface, Color.WHITE, pygame.Rect(self._x, self._y, self.width, self.height))
-        pygame.draw.rect(surface, Color.ALMOST_BLACK, pygame.Rect(self._x + 2, self._y + 2, self.width - 4, self.height - 4))
+        pygame.draw.rect(surface, Color.WHITE, pygame.Rect(self.x, self.y, self.width, self.height), border_radius=3)
+        pygame.draw.rect(surface, Color.ALMOST_BLACK, pygame.Rect(self.x + 2, self.y + 2, self.width - 4, self.height - 4), border_radius=3)
         surface.blit(self._image, (self._x + 30, self._y + 98))
+
+        self._game_over_label.view.setXY((self.width - self._game_over_label.view.width) / 2 + self.x, self.y + 20)
+        self._game_over_label.render(surface)
+
+        self._explanation_label.view.setXY(
+            (self.width - self._explanation_label.view.width) / 2 + self.x,
+            self._game_over_label.view.y + self._game_over_label.view.height + 5,
+        )
+        self._explanation_label.render(surface)
--- src/lib/ui/fonts.py
+++ src/lib/ui/fonts.py
@@ -3,4 +3,6 @@ import pygame
 pygame.font.init()
 
 FONT_ARIAL_10 = pygame.font.SysFont('Arial', 10)
+FONT_ARIAL_16 = pygame.font.SysFont('Arial', 16)
 FONT_ARIAL_20 = pygame.font.SysFont('Arial', 20)
+FONT_ARIAL_30 = pygame.font.SysFont('Arial', 30)
