afce6ecbf363940cefd8c3949e3061f215abade6
Pier-Luc Brault <pier-luc@brault.me>
2022-11-26 22:32:41 -0500

    Handle animation properly on game over

--- src/game.py
+++ src/game.py
@@ -124,10 +124,12 @@ class Game:
     def _update(self, current_time):
         events = []
 
+        display_game_over_dialog = self._game_over and self._game_over_time is not None and current_time - self._game_over_time > 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if self._game_over and current_time - self._game_over_time >= 1000:
+            if self._game_over and display_game_over_dialog:
                 if event.type == pygame.MOUSEBUTTONUP or event.type == pygame.KEYUP:
                     self._setup()
             elif not self._game_over:
@@ -138,19 +140,19 @@ class Game:
         if self._game_over:
             if self._game_over_time is None:
                 self._game_over_time = current_time
-            elif current_time - self._game_over_time >= 1000 and self._game_over_dialog is None:
+            elif display_game_over_dialog and self._game_over_dialog is None:
                 self._game_over_dialog = GameOverDialog()
                 self._game_over_dialog.view.setXY(
                     (self._window_width - self._game_over_dialog.view.width) / 2, (self._window_height - self._game_over_dialog.view.height) / 2
                 )
                 self._game_objects.append(self._game_over_dialog)
-        if not self._game_over or current_time - self._game_over_time < 1000:
+        else:
             if current_time > self._last_new_process_check + (30000 if self._next_pid > 12 else 50):
                 self._last_new_process_check = current_time
                 self._create_process()
 
-            for game_object in self._game_objects:
-                game_object.update(current_time, events)
+        for game_object in self._game_objects:
+            game_object.update(current_time, events)
 
     def _render(self):
         self._screen.fill(Color.BLACK)
