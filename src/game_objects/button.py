e12db45d391c59aea74569625544ef2b3a6df0cf
Pier-Luc Brault <pier-luc@brault.me>
2023-07-27 12:54:20 -0400

    WIP button

--- src/game_objects/button.py
+++ src/game_objects/button.py
@@ -5,7 +5,7 @@ from game_objects.views.button_view import ButtonView
 class Button(GameObject):  
     
     def __init__(self, text, action_fn):
-        super().__init__(ButtonView(self))
         self.text = text
         self._action_fn = action_fn
+        super().__init__(ButtonView(self))
     
--- src/game_objects/views/button_view.py
+++ src/game_objects/views/button_view.py
@@ -8,14 +8,18 @@ class ButtonView(Drawable):
     def __init__(self, button):
         self._button = button
         super().__init__()
+        
+        self._text_surface = FONT_PRIMARY_MEDIUM.render(self._button.text, False, Color.WHITE)
 
     @property
     def width(self):
-        return 0
+        return self._text_surface.get_width() + 24
 
     @property
     def height(self):
-        return 0
+        return self._text_surface.get_height() + 24
 
     def draw(self, surface):
-        pygame.draw.rect(surface, Color.WHITE, pygame.Rect(self.x, self.y, 200, 50), border_radius=3)
+        pygame.draw.rect(surface, Color.WHITE, pygame.Rect(self.x, self.y, self.width, self.height), border_radius=3)
+        pygame.draw.rect(surface, Color.ALMOST_BLACK, pygame.Rect(self.x + 2, self.y + 2, self.width - 4, self.height - 4), border_radius=3)
+        surface.blit(self._text_surface, (self.x + 12, self.y + 12))
