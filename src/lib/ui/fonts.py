47249ba9a761185e6928046bcf930e0a516a95e1
Pier-Luc Brault <pier-luc@brault.me>
2023-07-27 17:46:01 -0400

    Use path.join for file paths

--- src/lib/ui/fonts.py
+++ src/lib/ui/fonts.py
@@ -1,12 +1,13 @@
+from os import path
 import pygame
 
 pygame.font.init()
 
-FONT_PRIMARY_SMALL = pygame.font.Font('assets/VT323-Regular.ttf', 16) 
-FONT_PRIMARY_MEDIUM = pygame.font.Font('assets/VT323-Regular.ttf', 20)
-FONT_PRIMARY_LARGE = pygame.font.Font('assets/VT323-Regular.ttf', 24)
-FONT_PRIMARY_XLARGE = pygame.font.Font('assets/VT323-Regular.ttf', 30)
-FONT_PRIMARY_XXLARGE = pygame.font.Font('assets/VT323-Regular.ttf', 40)
+FONT_PRIMARY_SMALL = pygame.font.Font(path.join('assets', 'VT323-Regular.ttf'), 16) 
+FONT_PRIMARY_MEDIUM = pygame.font.Font(path.join('assets', 'VT323-Regular.ttf'), 20)
+FONT_PRIMARY_LARGE = pygame.font.Font(path.join('assets', 'VT323-Regular.ttf'), 24)
+FONT_PRIMARY_XLARGE = pygame.font.Font(path.join('assets', 'VT323-Regular.ttf'), 30)
+FONT_PRIMARY_XXLARGE = pygame.font.Font(path.join('assets', 'VT323-Regular.ttf'), 40)
 
-FONT_SECONDARY_XXSMALL = pygame.font.Font('assets/VictorMono-Regular.ttf', 8)
-FONT_SECONDARY_XSMALL = pygame.font.Font('assets/VictorMono-Regular.ttf', 9)
+FONT_SECONDARY_XXSMALL = pygame.font.Font(path.join('assets', 'VictorMono-Regular.ttf'), 8)
+FONT_SECONDARY_XSMALL = pygame.font.Font(path.join('assets', 'VictorMono-Regular.ttf'), 9)
