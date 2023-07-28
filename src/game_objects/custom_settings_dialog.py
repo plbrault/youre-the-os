6f17ab2044c3a35c2eda1db601195ced2ad4a0ab
Pier-Luc Brault <pier-luc@brault.me>
2023-07-28 16:30:58 -0400

    First label and selector

--- src/game_objects/custom_settings_dialog.py
+++ src/game_objects/custom_settings_dialog.py
@@ -1,13 +1,25 @@
+from difficulty_levels import default_difficulty
 from lib.game_object import GameObject
 from game_objects.button import Button
+from game_objects.option_selector import OptionSelector
 from game_objects.views.custom_settings_dialog_view import CustomSettingsDialogView
 
 class CustomSettingsDialog(GameObject):
     
     def __init__(self):
         super().__init__(CustomSettingsDialogView(self))
+        
+        self._config = default_difficulty['config']
+        
+        self._num_cpus_selector = OptionSelector([str(i) for i in range(1, 13)], self._config['num_cpus'] - 1)
+        self.children.append(self._num_cpus_selector)
 
-    def update(self, current_time, events):  
+    def update(self, current_time, events):
+        self._num_cpus_selector.view.set_xy(
+            self.view.x + self.view.width - self._num_cpus_selector.view.width - 10,
+            self.view.num_cpus_y + (self.view.num_cpus_height - self._num_cpus_selector.view.height) / 2
+        )
+        
         for child in self.children:
             child.update(current_time, events)
     
--- src/game_objects/views/custom_settings_dialog_view.py
+++ src/game_objects/views/custom_settings_dialog_view.py
@@ -2,14 +2,18 @@ from os import path
 import pygame
 
 from lib.drawable import Drawable
+from lib.ui.fonts import FONT_PRIMARY_LARGE, FONT_SECONDARY_MEDIUM
 from lib.ui.color import Color
-from lib.ui.fonts import FONT_PRIMARY_LARGE, FONT_PRIMARY_XLARGE, FONT_PRIMARY_XXLARGE
+from lib.ui.fonts import FONT_PRIMARY_XXLARGE, FONT_SECONDARY_MEDIUM
 
 class CustomSettingsDialogView(Drawable):
     def __init__(self, custom_settings_dialog):
         self._custom_settings_dialog = custom_settings_dialog
         super().__init__()
-
+        
+        self._title_text = FONT_PRIMARY_XXLARGE.render('Custom Settings', True, Color.WHITE)
+        self._num_cpus_label_text = FONT_SECONDARY_MEDIUM.render('Number of CPUs', True, Color.WHITE)
+        
     @property
     def width(self):
         return 400
@@ -17,7 +21,18 @@ class CustomSettingsDialogView(Drawable):
     @property
     def height(self):
         return 400
+    
+    @property
+    def num_cpus_y(self):
+        return self.y + self._title_text.get_height() + 40
+    
+    @property
+    def num_cpus_height(self):
+        return self._num_cpus_label_text.get_height()
 
     def draw(self, surface):
         pygame.draw.rect(surface, Color.WHITE, pygame.Rect(self.x, self.y, self.width, self.height), border_radius=3)
         pygame.draw.rect(surface, (70, 70, 70), pygame.Rect(self.x + 2, self.y + 2, self.width - 4, self.height - 4), border_radius=3)
+        
+        surface.blit(self._title_text, (self.x + (self.height - self._title_text.get_width()) / 2, self.y + 10))
+        surface.blit(self._num_cpus_label_text, (self.x + 10, self.num_cpus_y))
