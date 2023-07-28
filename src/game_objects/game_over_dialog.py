5aea68c57c1bce032129c4bd4cb14110cc7bd07f
Pier-Luc Brault <pier-luc@brault.me>
2023-07-27 22:28:15 -0400

    Create difficulty selector

--- src/game_objects/game_over_dialog.py
+++ src/game_objects/game_over_dialog.py
@@ -16,15 +16,14 @@ class GameOverDialog(GameObject):
         self.children.append(self._mainMenuButton)
 
     def update(self, current_time, events):
-        if self._playAgainButton.view.x == 0:
-            self._playAgainButton.view.set_xy(
-                self.view.x + 200,
-                self.view.y + self.view.height - self._playAgainButton.view.height - 20
-            )
-            self._mainMenuButton.view.set_xy(
-                self.view.x + self.view.width - self._mainMenuButton.view.width - 200,
-                self.view.y + self.view.height - self._playAgainButton.view.height - 20              
-            )
+        self._playAgainButton.view.set_xy(
+            self.view.x + 200,
+            self.view.y + self.view.height - self._playAgainButton.view.height - 20
+        )
+        self._mainMenuButton.view.set_xy(
+            self.view.x + self.view.width - self._mainMenuButton.view.width - 200,
+            self.view.y + self.view.height - self._playAgainButton.view.height - 20              
+        )
         
         for child in self.children:
             child.update(current_time, events)
--- src/scenes/main_menu.py
+++ src/scenes/main_menu.py
@@ -3,10 +3,12 @@ import pygame
 from random import randint
 import sys
 
+from difficulty_levels import difficulty_levels
 from lib.scene import Scene
 from lib.ui.color import Color
 from game_objects.button import Button
 from game_objects.main_menu_title import MainMenuTitle
+from game_objects.option_selector import OptionSelector
 
 class MainMenu(Scene):
     def __init__(self, screen, scenes):
@@ -18,6 +20,13 @@ class MainMenu(Scene):
         title = MainMenuTitle()
         title.view.set_xy(0, 50)
         self._game_objects.append(title)
+        
+        difficulty_selector = OptionSelector(
+            list(map(lambda difficulty_level: difficulty_level['name'], difficulty_levels)),
+            1
+        )
+        difficulty_selector.view.set_xy((self._screen.get_width() - difficulty_selector.view.width) / 2, title.view.y + title.view.height + 100)
+        self._game_objects.append(difficulty_selector)
         
         play_button = Button('Play Game', self._start_game)
         play_button.view.set_xy((self._screen.get_width() - play_button.view.width) / 2, title.view.y + title.view.height + 200)
