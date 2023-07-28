e05b1246f901da70894eb2fc8fa74f6062ceadf7
Pier-Luc Brault <pier-luc@brault.me>
2023-07-28 12:08:51 -0400

    Add difficulty level label

---
+++ src/game_objects/views/difficulty_selection_label_view.py
@@ -0,0 +1,24 @@
+from os import path
+import pygame
+
+from lib.ui.color import Color
+from lib.drawable import Drawable
+from lib.ui.fonts import FONT_SECONDARY_MEDIUM
+
+class DifficultySelectionLabelView(Drawable):
+    def __init__(self, difficulty_selection_label):
+        self._difficulty_selection_label = difficulty_selection_label
+        super().__init__()
+        
+        self._text = FONT_SECONDARY_MEDIUM.render("Select Difficulty :", True, Color.WHITE)
+
+    @property
+    def width(self):
+        return self._text.get_width()
+
+    @property
+    def height(self):
+        return self._text.get_height()
+
+    def draw(self, surface):
+        surface.blit(self._text, (self.x, self.y))
