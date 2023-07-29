316c4fb1a7d76921477718706894f87cde8fb2ea
Pier-Luc Brault <pier-luc@brault.me>
2023-07-28 21:32:26 -0400

    Fix display bug when there are no rows of swap pages

--- src/game_objects/views/page_manager_view.py
+++ src/game_objects/views/page_manager_view.py
@@ -4,7 +4,7 @@ from lib.ui.fonts import FONT_PRIMARY_LARGE
 
 class PageManagerView(Drawable):
     def __init__(self, page_manager):
-        self.page_manager = page_manager
+        self._page_manager = page_manager
         super().__init__()
         
         self._pages_in_ram_text_surface = FONT_PRIMARY_LARGE.render('Memory Pages in RAM :', False, Color.WHITE)
@@ -19,5 +19,6 @@ class PageManagerView(Drawable):
         return 768
 
     def draw(self, surface):
-        surface.blit(self._pages_in_ram_text_surface, self.page_manager.pages_in_ram_label_xy)
-        surface.blit(self._pages_in_swap_space_text_surface, self.page_manager.pages_in_swap_label_xy)
+        surface.blit(self._pages_in_ram_text_surface, self._page_manager.pages_in_ram_label_xy)
+        if self._page_manager.pages_in_swap_label_xy is not None:
+            surface.blit(self._pages_in_swap_space_text_surface, self._page_manager.pages_in_swap_label_xy)
