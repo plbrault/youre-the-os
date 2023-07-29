460026ef6a787c34a244022db386e5d18b97f14f
Pier-Luc Brault <pier-luc@brault.me>
2023-07-29 13:55:06 -0400

    Add previous and next buttons

--- src/scenes/how_to_play.py
+++ src/scenes/how_to_play.py
@@ -3,25 +3,63 @@ import pygame
 from random import randint
 
 from lib.ui.color import Color
+from game_objects.button import Button
 from game_objects.how_to_play_part import HowToPlayPart
 from lib.scene import Scene
 
 class HowToPlay(Scene):
     def __init__(self, screen, scenes):
         super().__init__(screen, scenes, background_color=Color.LIGHT_GREY)
+        
+        self._parts = []
+        self._current_part_id = 0
     
     def _setup(self):       
-        part1 = HowToPlayPart(
-            [
-                'In this game, you are the operating system of a computer.',
-                'You have to manage processes, memory, and input/output (I/O) events.'
-            ], 
-            [
-                path.join('assets', 'how_to_play_0_0.png')
-            ]
-        )
-        self._game_objects.append(part1)
-            
+        self._game_objects = []
+        
+        self._parts = [
+            HowToPlayPart(
+                [
+                    'In this game, you are the operating system of a computer.',
+                    'You have to manage processes, memory, and input/output (I/O) events.'
+                ], 
+                [
+                    path.join('assets', 'how_to_play_0_0.png')
+                ]
+            )
+        ]
+        
+        self._current_part_id = 0
+        self._game_objects.append(self._parts[self._current_part_id])
+        
+        previous_button = Button('<', self._go_to_previous_part)
+        previous_button.view.set_xy(50, 10)
+        self._game_objects.append(previous_button)
+        
+        next_button = Button('>', self._go_to_next_part)
+        next_button.view.set_xy(self._screen.get_width() - next_button.view.width - 50, 10)
+        self._game_objects.append(next_button)
+    
+    def _go_to_previous_part(self):
+        if self._current_part_id == 0:
+            self._return_to_main_menu()
+        else:
+            self._game_objects.remove(self._parts[self._current_part_id])
+            self._current_part_id -= 1
+            self._game_objects.append(self._parts[self._current_part_id])
+        
+    def _go_to_next_part(self):
+        if self._current_part_id == len(self._parts) - 1:
+            self._return_to_main_menu()
+        else:
+            self._game_objects.remove(self._parts[self._current_part_id])
+            self._current_part_id += 1
+            self._game_objects.append(self._parts[self._current_part_id])
+    
+    def _return_to_main_menu(self):
+        self.stop()
+        self._scenes['main_menu'].start()
+    
     def _update(self, current_time, events):
         for game_object in self._game_objects:
             game_object.update(current_time, events)
