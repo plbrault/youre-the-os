3c73fce32c1bde5156dd4f18a1f9eec7715a4560
Pier-Luc Brault <pier-luc@brault.me>
2023-07-30 17:07:09 -0400

    Customize web build

---
+++ build-web.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+
+build_only = False
+
+if len(sys.argv) > 1 and sys.argv[1] != '--build-only':
+    build_only = True
+
+command = [
+    'pygbag',
+    '--app_name', 'youre_the_os',
+    '--ume_block', '0',
+    '--title', '"You\'re the OS\\!"',
+]
+
+if build_only:
+    command.append('--build')
+
+command.append('src')
+
+subprocess.run(command)
