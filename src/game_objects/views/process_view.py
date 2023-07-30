4b7ddaa2ec0d6c03cbc7ac91f1949d27e98ba00a
Pier-Luc Brault <pier-luc@brault.me>
2023-07-29 23:22:44 -0400

    Local emojis

--- src/game_objects/views/process_view.py
+++ src/game_objects/views/process_view.py
@@ -1,5 +1,5 @@
+from os import path
 import pygame
-#from pygame_emojis import load_emoji
 
 from lib.drawable import Drawable
 from lib.ui.color import Color
@@ -15,20 +15,27 @@ _starvation_colors = [
     Color.DARK_GREY
 ]
 
-#_starvation_emoji_size = (28, 28)
-#_starvation_emojis = [
-#    load_emoji('😀', _starvation_emoji_size),
-#    load_emoji('🙂', _starvation_emoji_size),
-#    load_emoji('😐', _starvation_emoji_size),
-#    load_emoji('☹️', _starvation_emoji_size),
-#    load_emoji('😭', _starvation_emoji_size),
-#    load_emoji('🥶', _starvation_emoji_size),
-#    load_emoji('💀', _starvation_emoji_size),
-#]
+_emoji_size = (28, 28)
 
-#_gracefully_terminated_emoji = load_emoji('😇', _starvation_emoji_size)
+_starvation_emojis_full_size = [
+    pygame.image.load(path.join('assets', 'grinning_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'slightly_smiling_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'neutral_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'frowning_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'loudly_crying_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'cold_face_emoji.png')),
+    pygame.image.load(path.join('assets', 'skull_emoji.png')),
+]
 
-#_waiting_for_io_emoji = load_emoji('⏳', (28, 28))
+_starvation_emojis = list(map(lambda emoji: pygame.transform.scale(emoji, _emoji_size), _starvation_emojis_full_size))
+
+_gracefully_terminated_emoji_full_size = pygame.image.load(path.join('assets', 'smiling_face_with_halo_emoji.png'))
+
+_gracefully_terminated_emoji = pygame.transform.scale(_gracefully_terminated_emoji_full_size, _emoji_size)
+
+_waiting_for_io_emoji_full_size = pygame.image.load(path.join('assets', 'hourglass_not_done_emoji.png'))
+
+_waiting_for_io_emoji = pygame.transform.scale(_waiting_for_io_emoji_full_size, _emoji_size)
 
 class ProcessView(Drawable):
     def __init__(self, process):
@@ -69,17 +76,17 @@ class ProcessView(Drawable):
     def draw(self, surface):
         if self._process.has_ended and self._process.starvation_level == 0:
             color = Color.LIGHT_BLUE
-            #starvation_emoji_surface = _gracefully_terminated_emoji
+            starvation_emoji_surface = _gracefully_terminated_emoji
         else:
             color = _starvation_colors[self._process.starvation_level]
-            #starvation_emoji_surface = _starvation_emojis[self._process.starvation_level]
+            starvation_emoji_surface = _starvation_emojis[self._process.starvation_level]
             
         if self._process.display_blink_color:
             color = Color.BLUE
         
         pygame.draw.rect(surface, color, pygame.Rect(self._x, self._y, self.width, self.height))
-        #surface.blit(starvation_emoji_surface, (self._x, self._y + 2))
+        surface.blit(starvation_emoji_surface, (self._x, self._y + 2))
         surface.blit(self._pid_text_surface, (self._x + 28, self._y + 5))
 
-        #if self._process.is_waiting_for_io:
-        #    surface.blit(_waiting_for_io_emoji, (self._x + 27, self._y + 32))
+        if self._process.is_waiting_for_io:
+            surface.blit(_waiting_for_io_emoji, (self._x + 27, self._y + 32))
