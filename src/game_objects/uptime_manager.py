088af7007acc70a5c503ef603b30123dceecbaef
Pier-Luc Brault <pier-luc@brault.me>
2023-08-14 11:43:33 -0400

    Fix bug where uptime was including time when the game was paused (#66)

--- src/game_objects/uptime_manager.py
+++ src/game_objects/uptime_manager.py
@@ -8,16 +8,31 @@ class UptimeManager(GameObject):
     def __init__(self, game, current_time):
         self._game = game
         
-        self._start_time = current_time
-        self._uptime_text = '0:00:00'     
+        self._last_update_time = current_time
+        self._uptime = 0
+        self._uptime_text = '0:00:00'
+        
+        self._is_paused = False 
+        self._paused_time = 0
           
         super().__init__(UptimeManagerView(self))
     
     @property
     def uptime_text(self):
         return self._uptime_text
+    
+    def pause(self):
+        self._is_paused = True
+        self._paused_time = self._game.current_time
+        
+    def resume(self):
+        self._is_paused = False
+        self._last_update_time = self._game.current_time - (self._paused_time - self._last_update_time)
        
     def update(self, current_time, events):
-        uptime = current_time - self._start_time
-        self._uptime_text = str(timedelta(seconds=int(uptime / 1000)))
+        if not self._is_paused:
+            if current_time - self._last_update_time >= 1000:
+                self._last_update_time = current_time
+                self._uptime += 1000
+            self._uptime_text = str(timedelta(seconds=int(self._uptime / 1000)))
     
