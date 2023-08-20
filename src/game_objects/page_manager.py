6355297186816efc8e8b700c089a816062396a70
Pier-Luc Brault <pier-luc@brault.me>
2023-08-20 14:47:59 -0400

    Fix bug where page slots on disk didn't get cleared on process termination

--- src/game_objects/page_manager.py
+++ src/game_objects/page_manager.py
@@ -124,4 +124,8 @@ class PageManager(GameObject):
             if ram_slot.page == page:
                 ram_slot.page = None
                 break
+        for swap_slot in self._swap_slots:
+            if swap_slot.page == page:
+                swap_slot.page = None
+                break
         self.children.remove(page)
