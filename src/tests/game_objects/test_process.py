128400e694bb4607d0417f107a14506704e0d2c9
Pier-Luc Brault <pier-luc@brault.me>
2024-04-14 17:23:48 -0400

    Test initial values

--- src/tests/game_objects/test_process.py
+++ src/tests/game_objects/test_process.py
@@ -7,5 +7,15 @@ class TestProcess:
     def process(self, game):
         return Process(1, game)
 
-    def test_process(self, process):
+    def test_initial_property_values(self, process):
         assert process.pid == 1
+        assert process.has_cpu == False
+        assert process.is_waiting_for_io == False
+        assert process.is_waiting_for_page == False
+        assert process.is_blocked == False
+        assert process.has_ended == False
+        assert process.starvation_level == 1
+        assert process.display_blink_color == False
+        assert process.current_state_duration == 0
+        assert process.is_progressing_to_happiness == False
+
