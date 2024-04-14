b929ee9a9ca374c1fc7fa9cbb5b612dbd303d979
Pier-Luc Brault <pier-luc@brault.me>
2024-04-14 18:37:06 -0400

    WIP process test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,35 @@
 import os
 os.chdir('src')
 
+import pygame
 import pytest
 
 import scenes.game
+from window_size import WINDOW_SIZE
 
 @pytest.fixture
-def game():
-    return scenes.game.Game(None, None)
+def screen(monkeypatch):
+    screen = pygame.Surface(WINDOW_SIZE)
+    return screen
+
+@pytest.fixture
+def Game(monkeypatch):
+    @property
+    def current_time(self):
+        return 0
+    monkeypatch.setattr(scenes.game.Game, 'current_time', current_time)
+    return scenes.game.Game
+
+@pytest.fixture
+def game(Game, screen):
+    game_config = {
+        'name': 'Test Config',
+        'num_cpus': 4,
+        'num_processes_at_startup': 14,
+        'num_ram_rows': 8,
+        'new_process_probability': 0,
+        'io_probability': 0
+    }
+    game = Game(screen, None, game_config)
+    game.setup()
+    return game
--- src/tests/game_objects/test_process.py
+++ src/tests/game_objects/test_process.py
@@ -1,8 +1,13 @@
 import pytest
 
 from game_objects.process import Process
+from scenes.game import Game
 
 class TestProcess:
+    @pytest.fixture
+    def ProcessManager(self, monkeypatch):
+        pass
+
     @pytest.fixture
     def process(self, game):
         return Process(1, game)
@@ -19,3 +24,16 @@ class TestProcess:
         assert process.current_state_duration == 0
         assert process.is_progressing_to_happiness == False
 
+    def test_starvation_when_idle(self, process):
+        process.update(0, [])
+        assert process.starvation_level == 1
+        process.update(10000, [])
+        assert process.starvation_level == 2
+        process.update(20000, [])
+        assert process.starvation_level == 3
+        process.update(40000, [])
+        assert process.starvation_level == 4
+        process.update(50000, [])
+        assert process.starvation_level == 5
+
+        process.update(60000, [])       
