56f5bcd8a2e9ae6b52db872846dd9c75a1820f3a
Pier-Luc Brault <pier-luc@brault.me>
2024-04-14 17:17:11 -0400

    First test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,2 +1,10 @@
 import os
 os.chdir('src')
+
+import pytest
+
+import scenes.game
+
+@pytest.fixture
+def game():
+    return scenes.game.Game(None, None)
--- src/tests/game_objects/test_process.py
+++ src/tests/game_objects/test_process.py
@@ -3,6 +3,9 @@ import pytest
 from game_objects.process import Process
 
 class TestProcess:
-    def test_constructor(self):
-        p = Process()
-        assert p is not None
+    @pytest.fixture
+    def process(self, game):
+        return Process(1, game)
+
+    def test_process(self, process):
+        assert process.pid == 1
