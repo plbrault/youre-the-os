cb982a458d9fe0b59c57b46ace2efb83be0b1043
Pier-Luc Brault <pier-luc@brault.me>
2024-04-14 19:51:14 -0400

    text_max_starvation

--- src/tests/game_objects/test_process.py
+++ src/tests/game_objects/test_process.py
@@ -1,18 +1,15 @@
 import pytest
 
 from game_objects.process import Process
-from scenes.game import Game
 
 class TestProcess:
-    @pytest.fixture
-    def ProcessManager(self, monkeypatch):
-        pass
+    @property
+    def starvation_interval(self):
+        return 10000
 
-    @pytest.fixture
-    def process(self, game):
-        return Process(1, game)
+    def test_initial_property_values(self, game):
+        process = Process(1, game)
 
-    def test_initial_property_values(self, process):
         assert process.pid == 1
         assert process.has_cpu == False
         assert process.is_waiting_for_io == False
@@ -24,16 +21,25 @@ class TestProcess:
         assert process.current_state_duration == 0
         assert process.is_progressing_to_happiness == False
 
-    def test_starvation_when_idle(self, process):
-        process.update(0, [])
-        assert process.starvation_level == 1
-        process.update(10000, [])
-        assert process.starvation_level == 2
-        process.update(20000, [])
-        assert process.starvation_level == 3
-        process.update(40000, [])
-        assert process.starvation_level == 4
-        process.update(50000, [])
+    def test_starvation_when_idle(self, game):
+        process = Process(1, game)
+
+        for i in range(0, 5):
+            process.update(i * self.starvation_interval, [])
+            assert process.starvation_level == i + 1
+
+    def test_max_starvation(self, game, monkeypatch):
+        monkeypatch.setattr(game.process_manager, 'terminate_process', lambda process, by_user: True)
+        monkeypatch.setattr(game.process_manager, 'del_process', lambda process: None)
+
+        process = Process(1, game)
+
+        for i in range(0, 5):
+            process.update(i * self.starvation_interval, [])
+
         assert process.starvation_level == 5
 
-        process.update(60000, [])       
+        process.update(6 * self.starvation_interval, [])
+
+        assert process.starvation_level == 6
+        assert process.has_ended == True
