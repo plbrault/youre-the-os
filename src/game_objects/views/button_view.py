ccb110640f482897d3954e405c5ed64c1cf2801d
Pier-Luc Brault <pier-luc@brault.me>
2024-05-05 13:50:11 -0400

    Grey out button when disabled

--- src/game_objects/views/button_view.py
+++ src/game_objects/views/button_view.py
@@ -13,6 +13,8 @@ class ButtonView(Drawable):
         self._min_width = 0
         self._text_surface = FONT_PRIMARY_LARGE.render(
             self._button.text.upper(), False, Color.WHITE)
+        self._text_surface_disabled = FONT_PRIMARY_LARGE.render(
+            self._button.text.upper(), False, Color.GREY)
 
     @property
     def min_width(self):
@@ -31,16 +33,23 @@ class ButtonView(Drawable):
         return self._text_surface.get_height() + 24
 
     def draw(self, surface):
+        background_color = Color.ALMOST_BLACK
+        text_surface = self._text_surface
+
+        if self._button.disabled:
+            background_color = Color.DARK_GREY
+            text_surface = self._text_surface_disabled
+
         pygame.draw.rect(surface, Color.WHITE, pygame.Rect(
             self.x, self.y, self.width, self.height), border_radius=3)
         pygame.draw.rect(
             surface,
-            Color.ALMOST_BLACK,
+            background_color,
             pygame.Rect(
                 self.x + 2,
                 self.y + 2,
                 self.width - 4,
                 self.height - 4),
             border_radius=3)
-        surface.blit(self._text_surface, (self.x + (self.width -
-                     self._text_surface.get_width()) / 2, self.y + 12))
+        surface.blit(text_surface, (self.x + (self.width -
+                     text_surface.get_width()) / 2, self.y + 12))
