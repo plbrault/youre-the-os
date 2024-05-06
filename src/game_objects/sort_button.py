e1fab09f741066ad4f1b4a98cd466192a8c77f69
Pier-Luc Brault <pier-luc@brault.me>
2024-05-05 20:32:19 -0400

    Add cooldown on button

--- src/game_objects/sort_button.py
+++ src/game_objects/sort_button.py
@@ -2,6 +2,7 @@ from game_objects.button import Button
 from game_objects.views.sort_button_view import SortButtonView
 
 _BLINKING_INTERVAL_MS = 500
+_COOLDOWN_MS = 1000
 
 class SortButton(Button):
     def __init__(self, process_manager):
@@ -9,10 +10,13 @@ class SortButton(Button):
         self._process_manager = process_manager
         self._visible = False
         self._became_visible_at = 0
+        self._last_pressed_at = 0
         self._blinking = False
         self._blinking_hidden = False
 
     def _action(self):
+        self._last_pressed_at = self._process_manager.game.current_time
+        self.disabled = True
         self._blinking = self._blinking_hidden = False
         self._process_manager.sort_idle_processes()
 
@@ -43,4 +47,6 @@ class SortButton(Button):
             new_blinking_hidden = bool(int(visible_duration / _BLINKING_INTERVAL_MS) % 2 == 1)
             if new_blinking_hidden != self._blinking_hidden:
                 self._blinking_hidden = new_blinking_hidden
+        elif self.disabled and current_time - self._last_pressed_at >= _COOLDOWN_MS:
+            self.disabled = False
         super().update(current_time, events)
