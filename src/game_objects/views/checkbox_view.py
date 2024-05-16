ce61ff51a04a7d1318759863d7c90865e3faa1a7
Pier-Luc Brault <pier-luc@brault.me>
2024-05-16 19:51:01 -0400

    Working checkbox

--- src/game_objects/views/checkbox_view.py
+++ src/game_objects/views/checkbox_view.py
@@ -2,6 +2,9 @@ import pygame
 
 from engine.drawable import Drawable
 from ui.color import Color
+from ui.fonts import FONT_SECONDARY_MEDIUM
+
+_checkbox_image = pygame.image.load('assets/checkbox.png')
 
 
 class CheckboxView(Drawable):
@@ -11,12 +14,14 @@ class CheckboxView(Drawable):
 
     @property
     def width(self):
-        return 32
+        return 16
 
     @property
     def height(self):
-        return 32
+        return 16
 
     def draw(self, surface):
-        pygame.draw.rect(surface, Color.GREEN, pygame.Rect(
-            self._x, self._y, self.width, self.height))
+        pygame.draw.rect(surface, Color.LIME_GREEN, pygame.Rect(
+            self._x, self._y, self.width, self.height), 2)
+        if self._checkbox.checked:
+            surface.blit(_checkbox_image, (self._x + 2, self._y + 2))
