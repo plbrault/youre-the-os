74182af7f7de7a15d9314a82d974f49ccaff44fa
Pier-Luc Brault <pier-luc@brault.me>
2024-05-17 22:15:20 -0400

    Fix position

--- src/scenes/game.py
+++ src/scenes/game.py
@@ -75,9 +75,14 @@ class Game(Scene):
         )
         self._scene_objects.append(self._uptime_manager)
 
-        self._difficulty_label = Label(self._config['name'].upper())
+        self._difficulty_label = Label('Difficulty : ' + self._config['name'].upper())
         self._difficulty_label.view.set_xy(
-            (self._uptime_manager.view.x + self._score_manager.view.x + self._difficulty_label.view.width) // 2,
+            (
+                self._uptime_manager.view.x
+                + self._uptime_manager.view.width
+                + self._score_manager.view.x
+                - self._difficulty_label.view.width
+            ) // 2,
             self._score_manager.view.y
         )
         self._scene_objects.append(self._difficulty_label)
