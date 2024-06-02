d96234c3494ba7961e309abe1fa899a7fe028cd2
Pier-Luc Brault <pier-luc@brault.me>
2024-06-02 16:21:49 -0400

    Fix pylint

--- src/game_objects/views/priority_process_view.py
+++ src/game_objects/views/priority_process_view.py
@@ -6,9 +6,6 @@ from game_objects.views.process_view import ProcessView
 _icon = pygame.image.load(path.join('assets', 'crown_emoji.png'))
 
 class PriorityProcessView(ProcessView):
-    def __init__(self, process):
-        super().__init__(process)
-
     def draw(self, surface):
         super().draw(surface)
         surface.blit(_icon, (self._x + 2, self._y + 34))
