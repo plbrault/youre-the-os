fe4f4f97663ba0de0f527387e7fbb38ed4f25d4c
Pier-Luc Brault <pier-luc@brault.me>
2024-06-02 00:42:06 -0400

    Rename add_event to _add_event

--- src/game_monitor.py
+++ src/game_monitor.py
@@ -28,25 +28,25 @@ EventType = Enum('_et', [
 
 _events = []
 
-def add_event(typ, data):
+def _add_event(typ, data):
     _events.append(
         SimpleNamespace(etype=typ.name, **data)
     )
 
 def event_io_queue(count):
-    add_event(EventType.IO_QUEUE, {
+    _add_event(EventType.IO_QUEUE, {
         'io_count': count
     })
 
 def event_page_swap(pid, idx, swap):
-    add_event(EventType.PAGE_SWAP, {
+    _add_event(EventType.PAGE_SWAP, {
         'pid': pid,
         'idx': idx,
         'swap': swap
     })
 
 def event_page_new(pid, idx, swap, use):
-    add_event(EventType.PAGE_NEW, {
+    _add_event(EventType.PAGE_NEW, {
         'pid': pid,
         'idx': idx,
         'swap': swap,
@@ -54,59 +54,59 @@ def event_page_new(pid, idx, swap, use):
     })
 
 def event_page_use(pid, idx, use):
-    add_event(EventType.PAGE_USE, {
+    _add_event(EventType.PAGE_USE, {
         'pid': pid,
         'idx': idx,
         'use': use
     })
 
 def event_page_free(pid, idx):
-    add_event(EventType.PAGE_FREE, {
+    _add_event(EventType.PAGE_FREE, {
         'pid': pid,
         'idx': idx
     })
 
 def event_process_wait_page(pid, value):
-    add_event(EventType.PROC_WAIT_PAGE, {
+    _add_event(EventType.PROC_WAIT_PAGE, {
         'pid': pid,
         'waiting_for_page': value
     })
 
 def event_process_wait_io(pid, value):
-    add_event(EventType.PROC_WAIT_IO, {
+    _add_event(EventType.PROC_WAIT_IO, {
         'pid': pid,
         'waiting_for_io': value
     })
 
 def event_process_terminated(pid):
-    add_event(EventType.PROC_TERM, {
+    _add_event(EventType.PROC_TERM, {
         'pid': pid
     })
 
 def event_process_killed(pid):
-    add_event(EventType.PROC_KILL, {
+    _add_event(EventType.PROC_KILL, {
         'pid': pid
     })
 
 def event_process_starvation(pid, level):
-    add_event(EventType.PROC_STARV, {
+    _add_event(EventType.PROC_STARV, {
         'pid' : pid,
         'starvation_level': level
     })
 
 def event_process_new(pid):
-    add_event(EventType.PROC_NEW, {
+    _add_event(EventType.PROC_NEW, {
         'pid': pid
     })
 
 def event_process_cpu(pid, cpu):
-    add_event(EventType.PROC_CPU, {
+    _add_event(EventType.PROC_CPU, {
         'pid': pid,
         'cpu': cpu
     })
 
 def event_process_end(pid):
-    add_event(EventType.PROC_END, {
+    _add_event(EventType.PROC_END, {
         'pid': pid
     })
 
