46e774f0c928207890bc70684951608ecac59397
Pier-Luc Brault <pier-luc@brault.me>
2024-06-01 21:50:10 -0400

    Fix pylint

--- src/game_objects/views/process_view.py
+++ src/game_objects/views/process_view.py
@@ -56,7 +56,7 @@ class ProcessView(Drawable):
     def height(self):
         return 64
 
-    def draw(self, surface):      
+    def draw(self, surface):
         if self._process.has_ended and self._process.starvation_level == 0:
             color = Color.LIGHT_BLUE
             starvation_emoji_surface = _gracefully_terminated_emoji
