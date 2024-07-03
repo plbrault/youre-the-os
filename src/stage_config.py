9f1da9adda83a1e16ae1e295f3624d93b8ac2cc2
Pier-Luc Brault <pier-luc@brault.me>
2024-07-03 15:29:29 -0400

    Add swap_delay attribute to config

--- src/stage_config.py
+++ src/stage_config.py
@@ -8,6 +8,7 @@ class StageConfig:
     num_processes_at_startup: int = 14
     max_processes: int = MAX_PROCESSES
     num_ram_rows: int = 8
+    swap_delay: int = 0.5
     new_process_probability: float = 0.05
     priority_process_probability: float = 0.01
     io_probability: float = 0.01
