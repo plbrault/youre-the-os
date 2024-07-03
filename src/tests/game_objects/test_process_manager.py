b71d4ffc2a8e99295dcee78ffc7a36ccea07265f
Pier-Luc Brault <pier-luc@brault.me>
2024-07-02 20:06:02 -0400

    Working is in motion check

--- src/tests/game_objects/test_process_manager.py
+++ src/tests/game_objects/test_process_manager.py
@@ -111,7 +111,7 @@ class TestProcessManager:
                     process_count < stage.config.num_processes_at_startup
                     or process_in_motion > 0
                 )
-                and iteration_count < 100
+                and iteration_count < 100000
             ):
                 iteration_count += 1
                 process_manager.update(int(time), [])
@@ -121,6 +121,9 @@ class TestProcessManager:
                     process_slot for process_slot in process_manager.process_slots if process_slot.process is not None
                 ]
                 process_count = len(used_process_slots)
+                process_in_motion = len([
+                    process_slot for process_slot in used_process_slots if process_slot.process.is_in_motion
+                ])
 
             # Bring back normal number generator
             monkeypatch.setattr(Random, 'get_number', Random.get_number)
