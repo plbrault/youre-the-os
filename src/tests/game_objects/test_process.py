8c6a37ff75b141c811cd0696b57df05cd9d66769
Pier-Luc Brault <pier-luc@brault.me>
2024-08-07 18:21:06 -0400

    Fix other test

--- src/tests/game_objects/test_process.py
+++ src/tests/game_objects/test_process.py
@@ -410,11 +410,13 @@ class TestProcess:
         process.use_cpu()
 
         stage.page_manager.get_page(1, 0).request_swap()
+        stage.page_manager.update(1, [])
         stage.page_manager.update(1000, [])
         process.update(0, [])
         assert process.is_blocked == True
 
         stage.page_manager.get_page(1, 0).request_swap()
+        stage.page_manager.update(1001, [])
         stage.page_manager.update(2000, [])
         process.update(0, [])
 
