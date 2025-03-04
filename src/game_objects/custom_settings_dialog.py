0afb1c8186d0a8a8a60ca4a82f4bab2c9bdf15ec
Pier-Luc Brault <pier-luc@brault.me>
2025-03-04 17:04:34 -0500

    Update custom settings dialog

--- src/game_objects/custom_settings_dialog.py
+++ src/game_objects/custom_settings_dialog.py
@@ -11,12 +11,7 @@ from game_objects.option_selector import OptionSelector
 from game_objects.views.custom_settings_dialog_view import CustomSettingsDialogView
 from stage_config import StageConfig
 
-_swap_delay_ms_to_ids = {
-    SWAP_DELAY_NAMES_TO_MS[SWAP_DELAY_NAMES[0]]: 0,
-    SWAP_DELAY_NAMES_TO_MS[SWAP_DELAY_NAMES[1]]: 1,
-    SWAP_DELAY_NAMES_TO_MS[SWAP_DELAY_NAMES[2]]: 2,
-    SWAP_DELAY_NAMES_TO_MS[SWAP_DELAY_NAMES[3]]: 3
-}
+_swap_delay_ms_to_ids = {SWAP_DELAY_NAMES_TO_MS[name]: idx for idx, name in enumerate(SWAP_DELAY_NAMES)}
 
 class CustomSettingsDialog(GameObject):
 
