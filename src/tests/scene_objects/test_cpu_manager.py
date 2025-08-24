f4e82e3728213c7cc99094512338dea20268d455
Pier-Luc Brault <pier-luc@brault.me>
2025-08-24 12:39:19 -0400

    Setup process creation fixture in CpuManager tests

--- src/tests/scene_objects/test_cpu_manager.py
+++ src/tests/scene_objects/test_cpu_manager.py
@@ -1,7 +1,11 @@
 import pytest
 
 from config.cpu_config import CpuConfig
+from config.stage_config import StageConfig
+from factories.process_factory import ProcessFactory
 from scene_objects.cpu_manager import CpuManager
+from scene_objects.process_manager import ProcessManager
+from scenes.stage import Stage
 
 class TestCPUManager:
     @pytest.fixture
@@ -22,7 +26,26 @@ class TestCPUManager:
             penalty_ms=1000,
         )
 
-    def test_get_cpu_no_hyperthreading(self, cpu_config_no_hyperthreading):
+    @pytest.fixture
+    def create_process(self, monkeypatch):
+        stage_config = StageConfig()
+        stage = Stage('test_stage', stage_config)
+
+        the_cpu_manager = None
+        def get_cpu_manager(self):
+            return the_cpu_manager
+        monkeypatch.setattr(ProcessManager, 'cpu_manager', property(get_cpu_manager))
+
+        process_factory = ProcessFactory(stage, stage_config)
+        process_manager = ProcessManager(stage, stage_config)
+        monkeypatch.setattr(Stage, 'process_manager', process_manager)
+
+        def create_process_fn(cpu_manager, pid: int):
+            the_cpu_manager = cpu_manager
+            return process_factory.create_standard_process(pid)
+        return create_process_fn
+
+    def test_get_cpu_with_no_hyperthreading(self, cpu_config_no_hyperthreading):
         cpu_manager = CpuManager(cpu_config_no_hyperthreading)
         cpu_manager.setup()
 
@@ -36,7 +59,7 @@ class TestCPUManager:
         assert cpu_manager.get_cpu_by_logical_id(4).logical_id == 4
         assert cpu_manager.get_cpu_by_logical_id(5) is None
 
-    def test_get_cpu_hyperthreading(self, cpu_config_hyperthreading):
+    def test_get_cpu_with_hyperthreading(self, cpu_config_hyperthreading):
         cpu_manager = CpuManager(cpu_config_hyperthreading)
         cpu_manager.setup()
 
@@ -57,3 +80,14 @@ class TestCPUManager:
         assert cpu_manager.get_cpu_by_logical_id(8).physical_id == 4
         assert cpu_manager.get_cpu_by_logical_id(8).logical_id == 8
         assert cpu_manager.get_cpu_by_logical_id(9) is None
+
+    def test_select_free_cpu_with_no_hyperthreading(self, cpu_config_no_hyperthreading, create_process):
+        cpu_manager = CpuManager(cpu_config_no_hyperthreading)
+        cpu_manager.setup()
+
+        process1 = create_process(cpu_manager, 1)
+        process2 = create_process(cpu_manager, 2)
+        process3 = create_process(cpu_manager, 3)
+
+        cpu = cpu_manager.select_free_cpu()
+        assert cpu.logical_id == 1
