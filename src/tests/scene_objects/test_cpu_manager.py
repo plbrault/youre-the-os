8c73d3aa0aed781e1ff5ce049bdd1849566bf0ac
Pier-Luc Brault <pier-luc@brault.me>
2025-08-24 11:44:14 -0400

    WIP tests

--- src/tests/scene_objects/test_cpu_manager.py
+++ src/tests/scene_objects/test_cpu_manager.py
@@ -35,3 +35,25 @@ class TestCPUManager:
         assert cpu_manager.get_cpu_by_logical_id(4).physical_id == 4
         assert cpu_manager.get_cpu_by_logical_id(4).logical_id == 4
         assert cpu_manager.get_cpu_by_logical_id(5) is None
+
+    def test_get_cpu_hyperthreading(self, cpu_config_hyperthreading):
+        cpu_manager = CpuManager(cpu_config_hyperthreading)
+        cpu_manager.setup()
+
+        assert cpu_manager.get_cpu_by_logical_id(1).physical_id == 1
+        assert cpu_manager.get_cpu_by_logical_id(1).logical_id == 1
+        assert cpu_manager.get_cpu_by_logical_id(2).physical_id == 1
+        assert cpu_manager.get_cpu_by_logical_id(2).logical_id == 2
+        assert cpu_manager.get_cpu_by_logical_id(3).physical_id == 2
+        assert cpu_manager.get_cpu_by_logical_id(3).logical_id == 3
+        assert cpu_manager.get_cpu_by_logical_id(4).physical_id == 2
+        assert cpu_manager.get_cpu_by_logical_id(4).logical_id == 4
+        assert cpu_manager.get_cpu_by_logical_id(5).physical_id == 3
+        assert cpu_manager.get_cpu_by_logical_id(5).logical_id == 5
+        assert cpu_manager.get_cpu_by_logical_id(6).physical_id == 3
+        assert cpu_manager.get_cpu_by_logical_id(6).logical_id == 6
+        assert cpu_manager.get_cpu_by_logical_id(7).physical_id == 4
+        assert cpu_manager.get_cpu_by_logical_id(7).logical_id == 7
+        assert cpu_manager.get_cpu_by_logical_id(8).physical_id == 4
+        assert cpu_manager.get_cpu_by_logical_id(8).logical_id == 8
+        assert cpu_manager.get_cpu_by_logical_id(9) is None
