44fd978eeb0637821b973114eb2f90f1c5f447cb
Pier-Luc Brault <pier-luc@brault.me>
2026-04-10 22:19:50 -0400

    test: add comprehensive Stage test coverage

--- src/tests/scenes/test_stage.py
+++ src/tests/scenes/test_stage.py
@@ -1,6 +1,8 @@
 import pytest
 
 from constants import DEAD_STARVATION_LEVEL, ONE_SECOND, FRAMERATE
+from engine.game_event import GameEvent
+from engine.game_event_type import GameEventType
 from engine.game_manager import GameManager
 from engine.random import Random
 from config.cpu_config import CpuConfig
@@ -50,6 +52,67 @@ class TestStage:
             return stage
         return create_stage
 
+    def test_setup_initializes_scene_objects(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=4,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+        ))
+
+        assert stage.process_manager is not None
+        assert stage.page_manager is not None
+        assert stage._score_manager is not None
+        assert stage.uptime_manager is not None
+
+    def test_game_over_initially_false(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=4,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+        ))
+
+        assert not stage.game_over
+
+    def test_game_over_setter(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=4,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+        ))
+
+        assert not stage.game_over
+        stage.game_over = True
+        assert stage.game_over
+
+    def test_name_property(self):
+        stage = Stage('My Stage', StageConfig())
+        assert stage.name == 'My Stage'
+        stage.name = 'Renamed'
+        assert stage.name == 'Renamed'
+
+    def test_standalone_property(self):
+        stage = Stage('Test', StageConfig(), standalone=True)
+        assert stage.standalone
+        stage.standalone = False
+        assert not stage.standalone
+
+    def test_is_paused_initially_false(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=4,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+        ))
+
+        assert not stage.is_paused
+
     def test_game_over(self, stage_custom_config):
         stage = stage_custom_config(StageConfig(
             cpu_config=CpuConfig(num_cores=4),
@@ -85,3 +148,65 @@ class TestStage:
         stage.update(time, [])
 
         assert stage.game_over
+
+    def test_game_over_does_not_trigger_before_max_terminated(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=10,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+            time_ms_to_show_sort_button=0,
+        ))
+
+        process_manager = stage.process_manager
+
+        for i in range(1, 6):
+            process = process_manager.get_process(i)
+            time = 0
+            while process.starvation_level < DEAD_STARVATION_LEVEL:
+                process.update(time, [])
+                time += ONE_SECOND
+
+        process_in_motion = True
+        time = 1000
+        while process_in_motion:
+            process_manager.update(time, [])
+            process_in_motion = False
+            for scene_object in process_manager.children:
+                if hasattr(scene_object, 'is_in_motion') and scene_object.is_in_motion:
+                    process_in_motion = True
+                    break
+            time += ONE_SECOND / FRAMERATE
+
+        stage.update(time, [])
+
+        assert not stage.game_over
+
+    def test_game_over_does_not_trigger_while_processes_in_motion(self, stage_custom_config):
+        stage = stage_custom_config(StageConfig(
+            cpu_config=CpuConfig(num_cores=4),
+            num_processes_at_startup=10,
+            max_processes_terminated_by_user=1,
+            new_process_probability=0,
+            io_probability=0,
+            graceful_termination_probability=0,
+            time_ms_to_show_sort_button=0,
+        ))
+
+        process_manager = stage.process_manager
+        process = process_manager.get_process(1)
+        time = 0
+        while process.starvation_level < DEAD_STARVATION_LEVEL:
+            process.update(time, [])
+            time += ONE_SECOND
+
+        assert process_manager.user_terminated_process_count >= 1
+
+        any_in_motion = any(
+            hasattr(so, 'is_in_motion') and so.is_in_motion
+            for so in process_manager.children
+        )
+        if any_in_motion:
+            stage.update(time, [])
+            assert not stage.game_over
