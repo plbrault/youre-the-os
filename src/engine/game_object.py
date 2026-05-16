0e488febe5d2d120cd01438249382dec9d2583ec
Pier-Luc Brault <pier-luc@brault.me>
2026-05-16 15:14:19 -0400

    Fix pylint

--- src/engine/game_object.py
+++ src/engine/game_object.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class GameObject(ABC):
+    # pylint: disable=too-few-public-methods
+
     @abstractmethod
     def update(self, current_time, events):
         pass
